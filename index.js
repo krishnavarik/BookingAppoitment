b1d455ac04d923bb1f70830556527165970ef479
krishnavarik <94860416+krishnavarik@users.noreply.github.com>
2021-11-24 02:22:42 +0530

    addeded both files

---
+++ index.js
@@ -0,0 +1,11 @@
+function onSubmit(event){
+    event.preventDefault()
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    localStorage.setItem("name",name);
+    localStorage.setItem("email",email);
+}
+
+
